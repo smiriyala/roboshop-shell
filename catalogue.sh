83ccfd24acc1c98c3d67d65a24be7d7fb36f16f8
subbum <subbu@gmail.com>
2023-04-20 04:59:31 +0100

    catalogue updated

--- catalogue.sh
+++ catalogue.sh
@@ -1,7 +1,8 @@
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
 useradd roboshop
-mkdir /app 
+mkdir /app
+rm -rf /app/*
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
 cd /app 
 unzip /tmp/catalogue.zip
