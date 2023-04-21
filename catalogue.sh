b22f1e220065133edeb82af2e1363a3414a28795
subbum <subbu@gmail.com>
2023-04-21 13:11:17 +0100

    added logfile to mongo and catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,48 +1,48 @@
 source common.sh
 
 print_head "Configure NodeJs Repo"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>{log_file}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
 
 print_head "Install Nodejs"
-yum install nodejs -y &>>{log_file}
+yum install nodejs -y &>>${log_file}
 
 print_head "create roboshop user"
-useradd roboshop &>>{log_file}
+useradd roboshop &>>${log_file}
 
 print_head "create app directory"
-mkdir /app &>>{log_file}
+mkdir /app &>>${log_file}
 
 print_head "remove old log content"
-rm -rf /app/* &>>{log_file}
+rm -rf /app/* &>>${log_file}
 
 print_head "download app content"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>{log_file}
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
 cd /app 
 
 print_head "extracting app content"
-unzip /tmp/catalogue.zip &>>{log_file}
+unzip /tmp/catalogue.zip &>>${log_file}
 
 print_head "install nodeJs Dependents"
-npm install &>>{log_file}
+npm install &>>${log_file}
 
 print_head "copy config service to systemD service"
-cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>{log_file}
+cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
 
 print_head "Reload SystemD"
-systemctl daemon-reload &>>{log_file}
+systemctl daemon-reload &>>${log_file}
 
 print_head "enable catalogue service"
-systemctl enable catalogue &>>{log_file}
+systemctl enable catalogue &>>${log_file}
 
 print_head "Restart catalogue service"
-systemctl restart catalogue &>>{log_file}
+systemctl restart catalogue &>>${log_file}
 
 print_head "copy mongodb repo file"
-cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>{log_file}
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
 
 print_head "Intall mongodb client"
-yum install mongodb-org-shell -y &>>{log_file}
+yum install mongodb-org-shell -y &>>${log_file}
 
 print_head "Load Schema"
-mongo --host mongodb-dev.devopsforyou.online </app/schema/catalogue.js &>>{log_file}
+mongo --host mongodb-dev.devopsforyou.online </app/schema/catalogue.js &>>${log_file}
--- mongo.sh
+++ mongo.sh
@@ -2,18 +2,18 @@ source common.sh
 
 
 print_head "MongoDB repo file copying"
-cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>{log_file}
+cp ${code_dir}/configs/mongodb.repo /etc/yum.repos.d/mongo.repo &>>${log_file}
 
 print_head "MongoDB installation started"
-yum install mongodb-org -y &>>{log_file}
+yum install mongodb-org -y &>>${log_file}
 
 print_head "update mongodb listener address"
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>{log_file}
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>${log_file}
 
 print_head "enable mongodb"
-systemctl enable mongod &>>{log_file}
+systemctl enable mongod &>>${log_file}
 
 print_head "ReStarted Mongodb"
-systemctl restart mongod &>>{log_file}
+systemctl restart mongod &>>${log_file}
 
 #systemctl restart mongod
