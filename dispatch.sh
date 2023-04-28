c8de4c4bb82123899897d31aa35a7775de0e520d
subbum <subbu@gmail.com>
2023-04-28 21:23:51 +0100

    distpatch udpated

--- dispatch.sh
+++ dispatch.sh
@@ -1,7 +1,7 @@
 source common.sh
 
 roboshop_app_password=$1
-if [-z "${roboshop_app_password}"];then
+if [ -z "${roboshop_app_password}" ];then
     echo -e "\e[31mMissing roboshop user password\e[0m"
     exit 1
 fi
