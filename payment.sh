a113caad25a5df85e54569f99eac9d47fe52505d
subbum <subbu@gmail.com>
2023-04-28 09:03:59 +0100

    if chnged to fi

--- payment.sh
+++ payment.sh
@@ -2,10 +2,10 @@ source common.sh
 
 
 roboshop_app_password=$1
-if[-z "${roboshop_app_password}"]; then
+if [ -z "${roboshop_app_password}" ]; then
     echo -e "\e[31mMissing Payment app user password argument\e[0m"
     exit 1
-if
+fi
 
 component=payment
 python_payment
